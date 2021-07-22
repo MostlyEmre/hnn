661b3311b48cd75237eb673d0a11612edd3faef9
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-07-21 22:15:09 -0400

    Added "Don't have any favorites yet" section to Favorites component

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
+import { Link } from "react-router-dom";
 // import components
 import PostCard from "../components/PostCard";
 
 export default function Favorites({ favorites, setFavorites }) {
+  if (favorites.length === 0) {
+    return (
+      <div className="no-favorite-warning">
+        <p>
+          You don't have any favorites yet. Feel free to start with checking out the <Link to="/new">new posts</Link>!
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
       {favorites.map((favorite) => (
